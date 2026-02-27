2817a26717b7767ab09e0f8a6294fd249b969e6a
Hassan <md.hk786142@gmail.com>
2026-02-27 23:04:11 +0530

    Create 2093-A.cpp

---
+++ Solutions/2093-A.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<cmath>
+#include <vector>
+using namespace std;
+#include <string>
+int main(){ 
+   int t;
+   cin>>t;
+   for(int p = 1;p<=t;p++){
+    int k;
+    cin>>k;
+if ( k%2 == 0){
+    cout<<"NO"<<endl;
+} else {
+    cout<<"YES"<<endl;
+}   
+   }
+    return 0;
+}
+ 
