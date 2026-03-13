4adde1a283ad674507a25967f8cfb6b9505a2ea1
Hassan <md.hk786142@gmail.com>
2026-03-13 22:59:26 +0530

    Create 1831-A.cpp

---
+++ Solutions/1831-A.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<cmath>
+#include <vector>
+using namespace std;
+#include <string>
+ 
+int main(){ 
+   int t;
+   cin>>t;
+   while(t--){
+    int n;
+    cin>>n;
+    vector<int> a(n);
+    for(int i = 0 ; i < n ; i++){
+        cin>>a[i];
+    }
+    vector<int> b(n);
+    for(int i = 0 ; i < n ; i++){
+        b[i] = n+1 - a[i];
+        cout<<b[i]<<" ";
+    }
+    cout<<endl;
+   }
+    return 0;
+}
