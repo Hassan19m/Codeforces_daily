dc5a740b0193f5027125e168dd1f967b49547a88
Hassan <md.hk786142@gmail.com>
2026-03-14 16:13:25 +0530

    Create 1806-A.cpp

---
+++ Solutions/1806-A.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<cmath>
+#include <vector>
+using namespace std;
+#include <string>
+int main(){ 
+   int t;
+   cin>>t;
+   while(t--){
+    int a , b , c , d;
+    cin>> a >> b>> c >> d;
+    int steps;
+    if ( c > a+d-b || d < b+c-a ) {
+        cout<<-1<<endl;
+    }
+    else if (a>c && b>d){
+        cout<<-1<<endl;
+    } else {
+        steps = a - c + 2*(d-b);
+        cout<<steps<<endl;
+    } 
+   }
+    return 0;
+}
